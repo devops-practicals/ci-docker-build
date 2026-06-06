ee9189a8b0dd4d4631fa24827dbf47c3abe82786
cloudmlops <84102103+cloudmlops@users.noreply.github.com>
2026-06-06 13:04:36 +0530

    Update title and header for June 2026

--- docker_python/src/app.py
+++ docker_python/src/app.py
@@ -5,9 +5,9 @@ import numpy as np
 
 st.set_page_config(page_title="Financial Planning Calculator")
 
-st.title("Financial Planning Calculator-Nov-2025")
+st.title("Financial Planning Calculator-Jun-2026")
 
-st.header("**Monthly Income-Nov-2025**")
+st.header("**Monthly Income-Jun-2026**")
 st.subheader("Salary-Nov-2025")
 colAnnualSal, colTax = st.columns(2)
 
